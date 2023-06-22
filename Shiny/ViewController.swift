b50f6c3bf476f2fd30578f92dcdc274c7285a3ba
markfaust123 <markfaust123@gmail.com>
2023-06-22 09:22:50 -0400

    Initial Commit

---
+++ Shiny/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shiny
+//
+//  Created by Mark Faust on 6/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
